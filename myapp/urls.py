44ebfe030431040ef55681980176164bb6ec9723
nabin1200 <nabinbasnet1200@gmail.com>
2025-03-18 21:40:51 +0545

    views and templets

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from myapp.views import home
-
+from . import views
 urlpatterns=[
-    path("",home),
+    path("",views.index,name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+
 ]
